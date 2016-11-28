a43cc89df2e4862a6329f57b5f1ad3fb0122ec6f
Rogerio <Rogerio@DESKTOP-V2B7QR6>
2016-11-28 16:19:56 -0200

    javastdinandstdout2

---
+++ HackerRankExercises/src/javaExercises/JavaStdinAndStdoutII.java
@@ -0,0 +1,25 @@
+package javaExercises;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdoutII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		if(a%2==1) {
+			System.out.println("Weird");
+		}
+		else if (a%2 == 0 && 2 <= a && a <= 5) {
+			System.out.println("Not Weird");
+		}
+		else if (a%2 == 0 && 6 <= a && a <= 20) {
+			System.out.println("Weird");
+		}
+		else if (a%2 == 0 && a > 20) {
+			System.out.println("Not Weird");
+		}
+	}
+
+}
