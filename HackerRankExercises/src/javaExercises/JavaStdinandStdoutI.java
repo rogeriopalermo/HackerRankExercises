5d4ca41c39bc52fe6aaa74986e40037e637892f7
Rogerio <Rogerio@DESKTOP-V2B7QR6>
2016-11-28 16:19:29 -0200

    javastdinandstdoutii

--- HackerRankExercises/src/javaExercises/JavaStdinandStdoutI.java
+++ HackerRankExercises/src/javaExercises/JavaStdinandStdoutI.java
@@ -7,7 +7,7 @@ public class JavaStdinandStdoutI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 Scanner scan = new Scanner(System.in);
-	        int a = scan.nextInt();
+	     int a = scan.nextInt();
 	        int b = scan.nextInt();
 	        int c = scan.nextInt();
 
