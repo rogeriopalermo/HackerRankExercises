4d84a89c53c2b447d1a31bc21e2902bb2126ca2f
Familia <Familia@DESKTOP-4H2OI3I>
2016-12-10 18:36:28 -0200

    https://www.hackerrank.com/challenges/reverse-game

--- HackerRankExercises/src/mathematicsExercises/ReverseGame.java
+++ HackerRankExercises/src/mathematicsExercises/ReverseGame.java
@@ -4,6 +4,22 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class ReverseGame {
+	
+//	0 1 2 3 4 5 6		0 1 2 3 4 5 6 7 8 9 
+//						9 8 7 6 5 4 3 2 1 0
+//	6 5 4 3 2 1 0		9 0 1 2 3 4 5 6 7 8
+//						9 0 8 7 6 5 4 3 2 1
+//	6 0 1 2 3 4 5		9 0 8 1 2 3 4 5 6 7
+//						9 0 8 1 7 6 5 4 3 2
+//	6 0 5 4 3 2 1 		9 0 8 1 7 2 3 4 5 6
+//						9 0 8 1 7 2 6 5 4 3
+//	6 0 5 1 2 3 4		9 0 8 1 7 2 6 3 4 5
+//						9 0 8 1 7 2 6 3 5 4
+//	6 0 5 1 4 3 2
+//
+//	6 0 5 1 4 2 3
+//
+//	pattern (n-1) (n0) (n-2) (n1) (n-3) (n2) (n-4) (n3)...(n/2)
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
